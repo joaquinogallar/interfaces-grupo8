c2aa92a8386bad7cdf6b3e18282cfd386ebe7e30
luca <ochoa.luca@gmail.com>
2024-09-27 22:03:45 -0300

    prueba
    main
    router

---
+++ TP2/router.php
@@ -0,0 +1,22 @@
+<?php
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+$action = 'home'; // accion por defecto
+if (!empty( $_GET['action'])) {
+    $action = $_GET['action'];
+}
+
+
+// parsea la accion para separar accion real de parametros
+$params = explode('/', $action);
+
+switch ($params[0]) {
+    case 'home':
+        require_once "home.html";
+        break;
+    
+    default: 
+        echo "<p>404 Page Not Found</p> <a href='home'>Home</a>";
+        break;
+}
