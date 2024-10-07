38c7a5dcf537cab445274e6db066d411d91052e3
Joaquin Ogallar <joaquinnogallar@gmail.com>
2024-10-07 18:26:30 -0300

    feat: cambio de las url para el consumo de los juegos

--- TP2/src/main.js
+++ TP2/src/main.js
@@ -1,8 +1,20 @@
-import { cargarJuegos } from './cargarJuegos.js';
-import { initSwiper } from './initSwiper.js';
+import { cargarJuegos } from "./cargarJuegos.js";
+import { initSwiper } from "./initSwiper.js";
 
 const swiperDestacados = initSwiper("#swiper-destacados", 4.5);
 const swiperRecomendados = initSwiper("#swiper-recomendados", 6.5);
 
-cargarJuegos("/data/juegosDestacados.json", "carrucel-destacados", "tarjeta-destacado-container", true, swiperDestacados);
-cargarJuegos("/data/juegosRecomendados.json", "carrucel-recomendados", "tarjeta-recomendado-container", false, swiperDestacados);
+cargarJuegos(
+  "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegosDestacados.json",
+  "carrucel-destacados",
+  "tarjeta-destacado-container",
+  true,
+  swiperDestacados
+);
+cargarJuegos(
+  "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegosRecomendados.json",
+  "carrucel-recomendados",
+  "tarjeta-recomendado-container",
+  false,
+  swiperDestacados
+);
