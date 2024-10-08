2a6c059cc20a12bf423f6cc7af0460ad0f41dbf3
Joaco <joaquinnogallar@gmail.com>
2024-10-08 09:43:25 -0300

    feat: carrucel categoria accion

--- TP2/src/main.js
+++ TP2/src/main.js
@@ -2,7 +2,9 @@ import { cargarJuegos } from "./cargarJuegos.js";
 import { initSwiper } from "./initSwiper.js";
 
 const swiperDestacados = initSwiper("#swiper-destacados", 4.5);
-const swiperRecomendados = initSwiper("#swiper-recomendados", 6.5);
+const swiperRecomendados1 = initSwiper("#swiper-recomendados1", 6.5);
+const swiperRecomendados2 = initSwiper("#swiper-recomendados2", 6.5);
+const swiperCategoria1 = initSwiper("#swiper-categoria1", 6.5);
 
 cargarJuegos(
   "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json",
@@ -16,8 +18,26 @@ cargarJuegos(
 cargarJuegos(
   "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json",
   10,
-  "carrucel-recomendados",
+  "carrucel-recomendados1",
   "tarjeta-recomendado-container",
   false,
-  swiperRecomendados
+  swiperRecomendados1
+);
+
+cargarJuegos(
+  "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json",
+  10,
+  "carrucel-recomendados2",
+  "tarjeta-recomendado-container",
+  false,
+  swiperRecomendados2
+);
+
+cargarJuegos(
+  "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json",
+  10,
+  "carrucel-categoria1",
+  "tarjeta-recomendado-container",
+  false,
+  swiperRecomendados1
 );
