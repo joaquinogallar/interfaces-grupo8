e6f44ce143b4c0922cf1d06bca0251de991b0b29
Joaco <joaquinnogallar@gmail.com>
2024-10-10 15:10:13 -0300

    feat: titulo y descripcion del juego

--- TP2/src/paginaJuego.js
+++ TP2/src/paginaJuego.js
@@ -7,12 +7,16 @@ const juego = await obtenerJuegoPorId(id);
 console.log(juego);
 
 
-const nombreJuego = document.querySelector(".nombre-juego");
 const inicioJuego = document.querySelector(".inicio-juego")
-const tituloJuego = document.querySelector(".titulo-juego")
+const tituloJuego = document.querySelectorAll(".titulo-juego")
+const descJuego = document.querySelector(".descripcion-juego")
 
-nombreJuego.innerHTML = juego.nombre
 inicioJuego.style.background = `url(${juego.imagen})`
 inicioJuego.style.backgroundSize = "cover"
 inicioJuego.style.backgroundPosition = "center"
-tituloJuego.innerHTML = juego.nombre
+
+tituloJuego.forEach(titulo => {
+    titulo.innerHTML = juego.nombre;
+});
+
+descJuego.innerHTML = juego.descripcion;
