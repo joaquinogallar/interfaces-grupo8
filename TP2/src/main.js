96b0e95240c89115ad42fafe9f963c4c81748daf
Joaquin Ogallar <joaquinnogallar@gmail.com>
2024-10-11 08:23:17 -0300

    feat: varia el boton de los juegos segun si es pago, esta en el carrito o es gratis

--- TP2/src/main.js
+++ TP2/src/main.js
@@ -1,96 +1,21 @@
-import { cargarJuegos } from "./cargarJuegos.js";
-import { initSwiper } from "./initSwiper.js";
-/*
-const swiperDestacados = initSwiper(
-  "#swiper-destacados",
-  4.5,
-  3.7,
-  3.2,
-  2.9,
-  2.5,
-  1.8,
-  1
-);
-const swiperRecomendados1 = initSwiper(
-  "#swiper-recomendados1",
-  6.5,
-  5.5,
-  4.5,
-  3.5,
-  3.2,
-  2.5,
-  1.2
-);
-const swiperRecomendados2 = initSwiper(
-  "#swiper-recomendados2",
-  6.5,
-  5.5,
-  4.5,
-  3.5,
-  3.2,
-  2.5,
-  1.2
-);
-const swiperCategoria1 = initSwiper(
-  "#swiper-categoria1",
-  6.5,
-  5.5,
-  4.5,
-  3.5,
-  3.2,
-  2.5,
-  1.2
-);*/
-/*
-const swiperDestacados = initSwiper("#swiper-destacados", 4.5);
-const swiperRecomendados1 = initSwiper("#swiper-recomendados1", 6.5);
-const swiperRecomendados2 = initSwiper("#swiper-recomendados2", 6.5);
-const swiperCategoria1 = initSwiper("#swiper-categoria1", 6.5);*/
-
-
 /* SE CREA UN SWIPER DE swiper.js CON LOS NOMBRES DE LAS CLASES  */
-await newSwiper(".carrusel-destacados-div","tarjeta-destacado-container","destacado");
-
-await newSwiper(".carrusel-recomendados-div","tarjeta-recomendado-container","recomendado");
-
-await newSwiper(".carrusel-categoria1-div","tarjeta-categorias-container","categoria");
-
-/*
-cargarJuegos(
-  "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json",
-  6,
-  "carrusel-destacados",
+await newSwiper(
+  ".carrusel-destacados-div",
   "tarjeta-destacado-container",
-  true,
-  swiperDestacados
+  "destacado"
 );
 
-cargarJuegos(
-  "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json",
-  10,
-  "carrusel-recomendados1",
+await newSwiper(
+  ".carrusel-recomendados-div",
   "tarjeta-recomendado-container",
-  false,
-  swiperRecomendados1
+  "recomendado"
 );
 
-cargarJuegos(
-  "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json",
-  10,
-  "carrusel-recomendados2",
-  "tarjeta-recomendado-container",
-  false,
-  swiperRecomendados2
+await newSwiper(
+  ".carrusel-categoria1-div",
+  "tarjeta-categorias-container",
+  "categoria"
 );
-
-cargarJuegos(
-  "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json",
-  10,
-  "carrusel-categoria1",
-  "tarjeta-recomendado-container",
-  false,
-  swiperCategoria1
-);*/
 
 // lógica sidebar
 const sidebar = document.getElementById("sidebar");
--- TP2/src/swiper.js
+++ TP2/src/swiper.js
@@ -1,102 +1,113 @@
-async function newSwiper(idCarrusel, tarjeta,titulo) {
-    const carrusel = document.querySelector(idCarrusel);
+async function newSwiper(idCarrusel, tarjeta, titulo) {
+  const carrusel = document.querySelector(idCarrusel);
 
+  let juegos = await get(
+    "https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json"
+  );
 
-    let juegos = await get("https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json");
+  if (idCarrusel !== ".carrusel-destacados-div") {
+    juegos = juegos.sort(() => Math.random() - 0.5);
+  }
 
-    console.log(idCarrusel);
-    if(idCarrusel !== '.carrusel-destacados-div'){
-        console.log("desordenando");
-        juegos = juegos.sort(() => Math.random() - 0.5);
-    }
+  let i = 0;
 
-    let i = 0;
-    
+  if (juegos) {
+    for (const juego of juegos) {
+      i++;
+      let add = "<div class='swiper-slide' id='" + i + "'>";
+      let tipoBoton = "";
+      let textoBoton = "Jugar";
 
-    if (juegos) {
-        for(const juego of juegos) {
-            i++;
-            let add = "<div class='swiper-slide' id='" + i + "'>"; 
-            
-            add += "<div class='tarjeta-juego " + tarjeta +"' style='background-image:url(\"" + juego.imagen + "\")';>";
+      add +=
+        "<div class='tarjeta-juego " +
+        tarjeta +
+        "' style='background-image:url(\"" +
+        juego.imagen +
+        "\")';>";
 
-            if(juego.precio <= 0) {
-                juego.precio = null;
-            }
-
-            add += "<div class='juego-precio-container precio-"+ titulo +"'><span class='body0'>" + (juego.precio != null ? juego.precio : "") +"</span> </div>"
-
-            add += "<div class='juego-nombre-container'><h1 class='titulo-"+ titulo + " h1alt'>"+ juego.nombre +"</h1> <button class='boton-juego boton-"+ titulo + " body1' id='" + juego.id + "'>Jugar</button></div>";
-
-            add += "</div>";
-
-            carrusel.innerHTML += add;
-
-        };
-
-        
-        let slides = document.querySelectorAll(idCarrusel +' .swiper-slide');
-
-        let btnNext = carrusel.nextElementSibling;
-        let btnPrev = btnNext.nextElementSibling; 
-
-        let currentSlide = 0;
-        let totalSlides = slides.length;
-
-        
-        btnNext.addEventListener('click', function() {
-            let slide = carrusel.querySelector('.swiper-slide');
-            let slideWidth = slide.offsetWidth;
-
-            const maxSlidesVisible = Math.floor(carrusel.offsetWidth / slideWidth); 
-            
-            console.log(slideWidth);
-
-            /* Verifica que no se pase del total de slides visibles */
-            if (currentSlide < totalSlides - maxSlidesVisible) {
-                currentSlide++;
-                carrusel.style.transform = `translateX(-${currentSlide * slideWidth}px)`; /* Tamaño de slide*/
-            } else {
-                //console.log("No hay más slides a la derecha");
-            }
-        });
-        btnPrev.addEventListener('click', function() {   
-            let slide = carrusel.querySelector('.swiper-slide');
-            let slideWidth = slide.offsetWidth;
-
-            /* Verifica que no se pase del primer slide */
-            if (currentSlide > 0) {
-                currentSlide--;
-                carrusel.style.transform = `translateX(-${currentSlide * slideWidth}px)`; // Tamaño de slide
-            } else {
-                //console.log("No hay más slides a la izquierda");
-            }
-        });
-        
-
-        
-
-       
-
-    }
-
-    console.log(juegos);
-
-}
-
-
-
-async function get(url){
-    try {
-        response = await fetch(url);
-        if(!response.ok) {
-            throw "Error en el fetch";
+      if (juego.precio <= 0) {
+        juego.precio = null;
+      } else {
+        textoBoton = "Agregar al Carrito";
+        tipoBoton = "color-boton-pago";
+        if (juego.enCarrito) {
+          tipoBoton += " enCarrito";
+          textoBoton = "En Carrito";
         }
-        const data = await response.json();
-        return data;
-    }catch (error) {
-        console.error('Error:', error);
+      }
+
+      add +=
+        "<div class='juego-precio-container precio-" +
+        titulo +
+        "'><span class='body0'>" +
+        (juego.precio != null ? `$${juego.precio}` : "") +
+        "</span> </div>";
+
+      add += `
+      <div class=juego-nombre-container>
+        <h1 class="titulo-${titulo} h1alt">${juego.nombre}</h1>
+        <button class="boton-juego boton-${titulo} body1 ${tipoBoton}" id="${juego.id}">${textoBoton}</button>
+      `;
+
+      add += "</div></div>";
+
+      carrusel.innerHTML += add;
     }
-    
+
+    let slides = document.querySelectorAll(idCarrusel + " .swiper-slide");
+
+    let btnNext = carrusel.nextElementSibling;
+    let btnPrev = btnNext.nextElementSibling;
+
+    let currentSlide = 0;
+    let totalSlides = slides.length;
+
+    btnNext.addEventListener("click", function () {
+      let slide = carrusel.querySelector(".swiper-slide");
+      let slideWidth = slide.offsetWidth;
+
+      const maxSlidesVisible = Math.floor(carrusel.offsetWidth / slideWidth);
+
+      console.log(slideWidth);
+
+      /* Verifica que no se pase del total de slides visibles */
+      if (currentSlide < totalSlides - maxSlidesVisible) {
+        currentSlide++;
+        carrusel.style.transform = `translateX(-${
+          currentSlide * slideWidth
+        }px)`; /* Tamaño de slide*/
+      } else {
+        //console.log("No hay más slides a la derecha");
+      }
+    });
+    btnPrev.addEventListener("click", function () {
+      let slide = carrusel.querySelector(".swiper-slide");
+      let slideWidth = slide.offsetWidth;
+
+      /* Verifica que no se pase del primer slide */
+      if (currentSlide > 0) {
+        currentSlide--;
+        carrusel.style.transform = `translateX(-${
+          currentSlide * slideWidth
+        }px)`; // Tamaño de slide
+      } else {
+        //console.log("No hay más slides a la izquierda");
+      }
+    });
+  }
+
+  console.log(juegos);
 }
 
+async function get(url) {
+  try {
+    response = await fetch(url);
+    if (!response.ok) {
+      throw "Error en el fetch";
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
