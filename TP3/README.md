a4d9d881233622fc1f71d968d76e7139178afd9c
luca <ochoa.luca@gmail.com>
2024-10-17 20:02:33 -0300

    Tp3
    archivos copiados

---
+++ TP3/README.md
@@ -0,0 +1,5 @@
+<h2>TP 2</h2>
+<p><a href="home.html">Home
+<p><a href="login.html">Login
+<p><a href="register.html">Register
+<p><a href="juego.html?id=1">Juego
---
+++ TP3/src/obtenerJuegos.js
@@ -0,0 +1,62 @@
+export const obtenerJuegos = async (url) => {
+  try {
+    let response = await fetch(url);
+    if (response.ok) {
+      return await response.json();
+    } else {
+      console.error("No se pudieron obtener los datos de los juegos");
+      return [];
+    }
+  } catch (error) {
+    console.error("Error al obtener los datos: ", error);
+    return [];
+  }
+};
+
+export const obtenerJuegosRandom = async (url, cantidad) => {
+  try {
+    let juegos = await obtenerJuegos(url);
+    if (cantidad >= juegos.length) return juegos; // si la cantidad es mayor o igual que el número de juegos, devolver todos
+
+    let juegosSeleccionados = [];
+    let idsSeleccionados = new Set(); // set para evitar repetidos
+
+    let juegoId1 = juegos.find(juego => juego.id === 1);
+    if (juegoId1) {
+      juegosSeleccionados.push(juegoId1);
+      idsSeleccionados.add(juegoId1.id); // Añadir el id 1 al Set de seleccionados
+    }
+
+    while (juegosSeleccionados.length < cantidad) {
+      let indiceAleatorio = Math.floor(Math.random() * juegos.length);
+      let juegoAleatorio = juegos[indiceAleatorio];
+
+      if (!idsSeleccionados.has(juegoAleatorio.id)) { 
+        juegosSeleccionados.push(juegoAleatorio);
+        idsSeleccionados.add(juegoAleatorio.id); 
+      }
+    }
+
+    return juegosSeleccionados;
+  } catch (error) {
+    console.error("Error al obtener los juegos aleatorios: ", error);
+    return [];
+  }
+};
+
+export const obtenerJuegoPorId = async (id) => {
+  try {
+    let juegos = await obtenerJuegos("https://raw.githubusercontent.com/joaquinogallar/interfaces-grupo8/refs/heads/main/TP2/data/juegos.json");
+    
+    for (let juego of juegos) {
+      
+      if (juego.id == id) 
+        return juego;
+    }
+
+    return null;
+  } catch (error) {
+    console.error("Error al obtener los juegos aleatorios: ", error);
+    return null;
+  }
+}
